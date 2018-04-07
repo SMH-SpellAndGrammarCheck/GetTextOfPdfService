56ad18280087e14b9dca2b499ff89f6c87f649ff
Heiko Geppert <heiko.geppert@outlook.de>
2018-04-08 00:39:37 +0200

    adds some data handling into PdfHandler

--- src/PdfHandler.py
+++ src/PdfHandler.py
@@ -1,6 +1,7 @@
 import tika
 from tika import parser
-
+from azure.servicebus import ServiceBusService, Message, Queue
+import json
 
 class PdfHandler:
 
@@ -14,8 +15,30 @@ class PdfHandler:
         parsed = parser.from_file(file)
         print(parsed['metadata'])
         content = parsed['content']
+        split = content.replace('\n', '').split('.')
+        #print(content)
+
+        queue_data = load_queue_credentials()
+        print(queue_data['namespace'])
+        print(queue_data['key_name'])
+        print(queue_data['key_value'])
+        bus_service = ServiceBusService(
+            service_namespace=queue_data['namespace'],
+            shared_access_key_name=queue_data['key_name'],
+            shared_access_key_value=queue_data['key_value'])
+
+        #for part in split:
+        #send part to queue
+        print("sending...")
+        msg = Message('test')
+        bus_service.send_queue_message('texttoworker', msg)
+        print("done?")
         # split content
         # package content
         # send packages
 
         # todo return content
+
+def load_queue_credentials():
+    #todo change path
+    return json.load(open('D:/workspace/cloud hackathon/GetTextOfPdfService/src/queue.json'))
